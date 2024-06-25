e43485b623e8d3fc0d1a07859a64f470b4a2a5a2
LorenaSTN <lorenstn@hotmail.com>
2024-06-25 11:22:28 +0200

    ejercicio 2.3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const kittens = document.querySelector (".js-list");
 
-const kittenOne =        
+const kittenOne =
 `<li class="card kittenOne">
 <article>
   <img
@@ -20,7 +20,7 @@ const kittenOne =
 </article>
 </li>`;
 
-const kittenTwo = 
+const kittenTwo =
 `<li class="card kittenTwo">
 <img
   class="card_img"
@@ -36,12 +36,12 @@ const kittenTwo =
 </p>
 </li>`;
 
-const kittenThree = 
+const kittenThree =
 `<li class="card kittenThree">
 <img
   class="card_img"
   src="https://dev.adalab.es/maine-coon-cat.webp"
-  alt="maine-coon-cat" 
+  alt="maine-coon-cat"
 />
 <h3 class="card_title">Cielo</h3>
 <h4 class="card_race">Maine Coon</h4>
@@ -53,7 +53,7 @@ const kittenThree =
 </li>`;
 
 
-kittens.innerHTML =  kittenOne + kittenTwo + kittenThree; 
+kittens.innerHTML =  kittenOne + kittenTwo + kittenThree;
 
 const buttonAdd = document.querySelector(".button-js");
 const formSection = document.querySelector('.js-new-form');
@@ -62,7 +62,6 @@ const cancel= document.querySelector(".button-cancel");
 formSection.classList.remove('collapsed');
 
 buttonAdd.addEventListener("click",()=>{
-  console.log ("probando");
 formSection.classList.toggle("collapsed");
 }
 )
@@ -70,6 +69,62 @@ cancel.addEventListener("click", ()=>{
 formSection.classList.add("collapsed");
 
 }
-)
+);
+
+//Ejercicio 3:
+
+//
+
+const inputSearch= document.querySelector('.js_in_search_desc');
+const searchButton = document.querySelector (".js-button");
+const desc1 = `Porte elegante, su patrón de color tan característico y sus ojos
+de un azul intenso, pero su historia se remonta a Asía al menos
+hace 500 años, donde tuvo su origen muy posiblemente.`;
+const desc2 = `  Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+hasta con pinta de alienígena han llegado a definir a esta raza
+gatuna que se caracteriza por la «ausencia» de pelo.`;
+const desc3 = `  Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+bella mirada se ha convertido en una de sus señas de identidad.
+Sus ojos son grandes y las orejas resultan largas y en punta.`;
 
 
+
+searchButton.addEventListener("click", (event) => {
+event.preventDefault();
+ const descrSearchText = inputSearch.value;
+
+ if( descrSearchText.includes(desc1) ) {
+    kittens.innerHTML = kittenOne;
+  } else if ( descrSearchText.includes(desc2) ) {
+  kittens.innerHTML = kittenTwo;
+ } else if (descrSearchText.includes(desc3)) {
+  kittens.innerHTML = kittenThree;
+ }
+});
+
+
+
+
+const searchRaza = document.querySelector(".search-raza")
+const race = document.querySelector (".card-race")
+const raza1 = "siames";
+const raza2 = "sphynx";
+const raza3 = "";
+
+
+searchButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const inputRaza = searchRaza.value;
+
+  const race = "";
+
+  if (inputRaza = raza1) {
+    
+  }
+
+  message.innerHTML = race;
+})
+/*const siames = kittenOne;
+const sphynx = kittenTwo;
+const kittenThree = ""
